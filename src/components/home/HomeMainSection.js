c885d3291537b3947959b7a3d5278acdcb3ffad1
Adam <adamlapacz@gmail.com>
2021-07-31 01:56:55 +0200

    section home layout done

--- src/components/home/HomeMainSection.js
+++ src/components/home/HomeMainSection.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Hero from '../../assets/Home-Hero-Image.jpg';
+
 
 const HomeMainSection = () => {
     return (
-        <>
-        <h2>HomeMainSection</h2>
-        </>
+        <section className='home__main'>
+            <div className='home__main__img'>
+                <img className='home__main__img__item' src={Hero} alt='Oddaj rzeczy' />
+            </div>
+            <div className='home__main__description'>
+                <h1 className='home__main__description__title'>
+                    Zacznij pomagać!<br />
+                    Oddaj niechciane rzeczy w zaufane ręce
+                </h1>
+                <div className='home__main__description__btn'>
+                    <button className='home__main__description__btn__item'>oddaj<br />rzeczy</button>
+                    <button className='home__main__description__btn__item'>zorganizuj zbiórkę</button>
+                </div>
+            </div>
+        </section>
     )
 }
 
