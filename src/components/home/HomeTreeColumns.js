a5f75f24d7ad66dd75963c4737f5f257fd6ec51e
Adam <adamlapacz@gmail.com>
2021-08-01 21:17:38 +0200

    initial treeColumns structure

--- src/components/home/HomeTreeColumns.js
+++ src/components/home/HomeTreeColumns.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const HomeTreeColumns = () => {
     return (
-        <section id='info'>
-            <h2>HomeTreeColumns</h2>
+        <section className='columns' id='info'>
+           <div className='columns__container'>
+               <div className='columns__container__item'>
+                   <h2 className='columns__container__item__number'></h2>
+                   <h3 className='columns__container__item__title'></h3>
+                   <span className='columns__container__item__description'></span>
+               </div>
+               <div className='columns__container__item'>
+                   <h2 className='columns__container__item__number'></h2>
+                   <h3 className='columns__container__item__title'></h3>
+                   <span className='columns__container__item__description'></span>
+               </div>
+               <div className='columns__container__item'>
+                   <h2 className='columns__container__item__number'></h2>
+                   <h3 className='columns__container__item__title'></h3>
+                   <span className='columns__container__item__description'></span>
+               </div>
+           </div>
         </section>
     )
 }
