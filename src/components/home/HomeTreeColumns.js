de821107a1a9789bbae70d67c0aba70f8b7c759f
Adam <adamlapacz@gmail.com>
2021-08-02 16:24:42 +0200

    treeColumns styled

--- src/components/home/HomeTreeColumns.js
+++ src/components/home/HomeTreeColumns.js
@@ -5,19 +5,25 @@ const HomeTreeColumns = () => {
         <section className='columns' id='info'>
            <div className='columns__container'>
                <div className='columns__container__item'>
-                   <h2 className='columns__container__item__number'></h2>
-                   <h3 className='columns__container__item__title'></h3>
-                   <span className='columns__container__item__description'></span>
+                   <h2 className='columns__container__item__number'>10</h2>
+                   <h3 className='columns__container__item__title'>oddanych worków</h3>
+                   <span className='columns__container__item__description'>
+                       Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                   </span>
                </div>
                <div className='columns__container__item'>
-                   <h2 className='columns__container__item__number'></h2>
-                   <h3 className='columns__container__item__title'></h3>
-                   <span className='columns__container__item__description'></span>
+                   <h2 className='columns__container__item__number'>5</h2>
+                   <h3 className='columns__container__item__title'>wspartych organizacji</h3>
+                   <span className='columns__container__item__description'>
+                       Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                   </span>
                </div>
                <div className='columns__container__item'>
-                   <h2 className='columns__container__item__number'></h2>
-                   <h3 className='columns__container__item__title'></h3>
-                   <span className='columns__container__item__description'></span>
+                   <h2 className='columns__container__item__number'>7</h2>
+                   <h3 className='columns__container__item__title'>zorganizowanych zrbiórek</h3>
+                   <span className='columns__container__item__description'>
+                       Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+                   </span>
                </div>
            </div>
         </section>
