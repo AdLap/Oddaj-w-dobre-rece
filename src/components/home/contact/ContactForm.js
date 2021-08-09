f792d749ab365e6e98a852d2ea8c1d232607f50b
Adam <adamlapacz@gmail.com>
2021-08-09 09:36:13 +0200

    added contact form styled and validation ready

--- src/components/home/contact/ContactForm.js
+++ src/components/home/contact/ContactForm.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { ErrorMessage, Field, Formik } from 'formik';
 import * as Yup from 'yup';
-// import styles from './ContactForm.module.scss';
-// import CSSModules from 'react-css-modules';
-
 
 const ContactForm = () => {
     const styleForm = {
-        border: '1px solid red',
         width: '528px',
         height: '327px',
         marginTop: '128px',
@@ -18,15 +14,58 @@ const ContactForm = () => {
     }
 
     const styleInputBox = {
-        display: 'flex'
+        display: 'flex',
+        gap: '16px'
+    }
+
+    const styleLabel = {
+        fontFamily: '"Open Sans", sans-serif',
+        fontWeight: '600',
+        fontSize: '16px',
+        color: '#3C3C3C',
+        letterSpacing: '0',
+        width: '100%'
     }
 
     const styleInput = {
-        width: '50%'
+        width: '100%',
+        border: 'none',
+        background: 'transparent',
+        borderBottom: '.75px solid #3C3C3C',
+        fontFamily: '"Open Sans", sans-serif',
+        fontWeight: '400',
+        fontSize: '18px',
+        letterSpacing: '0',
+        color: '#3C3C3C',
+        marginTop: '12px'
     }
 
     const styleTextArea = {
-        width: '100%'
+        width: '100%',
+        height: '97px',
+        border: 'none',
+        background: 'transparent',
+        borderBottom: '.75px solid #3C3C3C',
+        fontFamily: '"Open Sans", sans-serif',
+        fontWeight: '400',
+        fontSize: '18px',
+        letterSpacing: '0',
+        color: '#3C3C3C',
+        marginTop: '12px',
+        resize: 'none'
+    }
+
+    const styleBtn = {
+        background: 'transparent',
+        border: '.75px solid #3C3C3C',
+        fontFamily: '"Open Sans", sans-serif',
+        fotnSize: '18px',
+        fontWeight: '300',
+        letterSpacing: '0',
+        color: '#000000',
+        padding: '13px 52px',
+        cursor: 'pointer',
+        alignSelf: 'flex-end'
     }
 
     return (
@@ -48,20 +87,40 @@ const ContactForm = () => {
         >
             {
                 ({ values, handleChange, handleSubmit }) => (
-                    <form onSubmit={handleSubmit} style={styleForm}>
-                        <label htmlFor='name'>Wpisz swoje imię</label>
-                        <Field name='name' type='text' style={styleInput} />
-                        <ErrorMessage name='name' component='div' />
+                    <form onSubmit={handleSubmit} style={styleForm} className='contact__form'>
+                        <div style={styleInputBox}>
+                            <label htmlFor='name' style={styleLabel}>Wpisz swoje imię
+                                <Field
+                                    name='name'
+                                    type='text'
+                                    style={styleInput}
+                                    placeholder='Krzysztof' />
+                                <ErrorMessage name='name' component='div' />
+                            </label>
 
-                        <label htmlFor='email'>Podaj swój adres email</label>
-                        <Field name='email' type='email' style={styleInput} />
-                        <ErrorMessage name='email' component='div' />
+                            <label htmlFor='email' style={styleLabel}>Podaj swój adres email
+                                <Field
+                                    name='email'
+                                    type='email'
+                                    style={styleInput}
+                                    placeholder='abc@xyz.pl' />
+                                <ErrorMessage name='email' component='div' />
+                            </label>
+                        </div>
 
-                        <label htmlFor='msg'>Wpisz wiadomość</label>
-                        <Field name='msg' type='textarea' style={styleTextArea} />
-                        <ErrorMessage name='msg' component='div' />
+                        <label htmlFor='msg' style={styleLabel}>Wpisz wiadomość
+                            <Field
+                                name='msg'
+                                as='textarea'
+                                rows='4'
+                                style={styleTextArea}
+                                placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                            aliquip ex ea commodo consequat.' />
+                            <ErrorMessage name='msg' component='div' />
+                        </label>
 
-                        <button type='submit'>Wyślij</button>
+                        <button type='submit' style={styleBtn}>Wyślij</button>
                     </form>
                 )
             }
