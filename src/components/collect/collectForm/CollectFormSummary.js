e6344e091b0d081e6cc591c0e328c9b660c8b164
Adam <adamlapacz@gmail.com>
2021-09-10 16:03:35 +0200

    collect summary style ready

--- src/components/collect/collectForm/CollectFormSummary.js
+++ src/components/collect/collectForm/CollectFormSummary.js
@@ -8,24 +8,51 @@ const CollectFormSummary = ({ prev, next, data }) => {
             <h2>Podsumowanie Twojej darowizny</h2>
             <div className='collect__form__summary'>
                 <div className='collect__form__summary__items'>
-                    <h3>Oddajesz:</h3>
-                    <div>{data.bags} worki, {data.type}, {data.helpGroups.join(', ')}</div>
-                    <div>dla lokalizacji: {data.localization}, {data.localizationSpecific}</div>
+                    <h3 className='collect__form__summary__title'>Oddajesz:</h3>
+                    <div className='collect__form__summary__items__desc'>
+                        <div className='collect__form__summary__icon__one'></div>
+                        {data.bags} worki, {data.type}, {data.helpGroups.join(', ')}
+                    </div>
+                    <div className='collect__form__summary__items__desc'>
+                        <div className='collect__form__summary__icon__two'></div>
+                        dla lokalizacji: {data.localization}, {data.localizationSpecific}
+                    </div>
                 </div>
 
                 <div className='collect__form__summary__adress'>
-                    <h3>Adres odbioru:</h3>
-                    <div>Ulica {data.street}</div>
-                    <div>Miasto {data.city}</div>
-                    <div>Kod pocztowy {data.postCode}</div>
-                    <div>Numer telefonu {data.phone}</div>
+                    <h3 className='collect__form__summary__title'>Adres odbioru:</h3>
+                    <div className='collect__form__summary__desc'>
+                        <span className='collect__form__summary__data'>Ulica</span>
+                        <span className='collect__form__summary__data'>{data.street}</span>
+                    </div>
+                    <div className='collect__form__summary__desc'>
+                        <span className='collect__form__summary__key'>Miasto</span>
+                        <span className='collect__form__summary__data'>{data.city}</span>
+                    </div>
+                    <div className='collect__form__summary__desc'>
+                        <span className='collect__form__summary__key'>Kod pocztowy</span>
+                        <span className='collect__form__summary__data'>{data.postCode}</span>
+                    </div>
+                    <div className='collect__form__summary__desc'>
+                        <span className='collect__form__summary__key'>Numer telefonu</span>
+                        <span className='collect__form__summary__data'>{data.phone}</span>
+                    </div>
                 </div>
 
                 <div className='collect__form__summary__date'>
-                    <h3>Termin odbioru</h3>
-                    <div>Data {data.date}</div>
-                    <div>Godzina {data.time}</div>
-                    <div>Uwagi dla kuriera {data.note}</div>
+                    <h3 className='collect__form__summary__title'>Termin odbioru</h3>
+                    <div className='collect__form__summary__desc'>
+                        <span className='collect__form__summary__key'>Data</span>
+                        <span className='collect__form__summary__data'>{data.date}</span>
+                    </div>
+                    <div className='collect__form__summary__desc'>
+                        <span className='collect__form__summary__key'>Godzina</span>
+                        <span className='collect__form__summary__data'>{data.time}</span>
+                    </div>
+                    <div className='collect__form__summary__desc'>
+                        <span className='collect__form__summary__key'>Uwagi<br />dla kuriera</span>
+                        <span className='collect__form__summary__data'>{data.note}</span>
+                    </div>
                 </div>
 
                 <div className='collect__form__btns'>
