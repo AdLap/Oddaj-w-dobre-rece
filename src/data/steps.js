141f1d5a6516c06b05fdd3753b74629bb52b6a04
Adam Łapacz <a.lapacz@global4net.com>
2023-03-15 04:16:09 +0100

    OD-17

---
+++ src/data/steps.js
@@ -0,0 +1,31 @@
+import Things from '../assets/Icon-1.svg'
+import Packing from '../assets/Icon-2.svg'
+import Choose from '../assets/Icon-3.svg'
+import Courier from '../assets/Icon-4.svg'
+
+export const steps = [
+	{
+		id: 1,
+		img: Things,
+		title: 'Wybierz rzeczy',
+		description: 'ubrania, zabawki, sprzęt i inne',
+	},
+	{
+		id: 2,
+		img: Packing,
+		title: 'Spakuj je',
+		description: 'skorzystaj z worków na śmieci',
+	},
+	{
+		id: 3,
+		img: Choose,
+		title: 'Zdecyduj komu chcesz pomóc',
+		description: 'wybierz zaufane miejsce',
+	},
+	{
+		id: 4,
+		img: Courier,
+		title: 'Zamów kuriera',
+		description: 'kurier przyjedzie w dogodnym terminie',
+	},
+]
