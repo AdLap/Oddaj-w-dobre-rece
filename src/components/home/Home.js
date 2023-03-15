334af0a2b0b96b651ac695178a94718e28d214ed
Adam Łapacz <a.lapacz@global4net.com>
2023-03-15 02:05:07 +0100

    OD-16

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import HomeAboutUs from './HomeAboutUs';
-import HomeContact from './HomeContact';
-import HomeFooter from './HomeFooter';
-import HomeFourSteps from './HomeFourSteps';
-import HomeHeader from './header/HomeHeader';
-import HomeMainSection from './main/HomeMainSection';
-import HomeThreeColumns from './HomeThreeColumns';
-import HomeWhoWeHelp from './HomeWhoWeHelp';
+import React from 'react'
+import HomeAboutUs from './HomeAboutUs'
+import HomeContact from './HomeContact'
+import HomeFooter from './HomeFooter'
+import HomeFourSteps from './HomeFourSteps'
+import HomeHeader from './header/HomeHeader'
+import HomeMainSection from './main/HomeMainSection'
+import HomeThreeColumns from './threeColumns/HomeThreeColumns'
+import HomeWhoWeHelp from './HomeWhoWeHelp'
 
 const Home = () => {
-    return (
-        <>
-            <HomeHeader />
-            <HomeMainSection />
-            <HomeThreeColumns />
-            <HomeFourSteps />
-            <HomeAboutUs />
-            <HomeWhoWeHelp />
-            <HomeContact />
-            <HomeFooter />
-        </>
-    )
+	return (
+		<>
+			<HomeHeader />
+			<HomeMainSection />
+			<HomeThreeColumns />
+			<HomeFourSteps />
+			<HomeAboutUs />
+			<HomeWhoWeHelp />
+			<HomeContact />
+			<HomeFooter />
+		</>
+	)
 }
 
-export default Home;
+export default Home
